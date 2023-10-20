3400e8dc3bc5fc4afa937d1320704ee331d3c133
MinukaF <142361876+MinukaF@users.noreply.github.com>
2023-10-20 22:50:43 +1100

    Add files via upload

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,15 +1,29 @@
 const mongoose = require('mongoose');
 
-const orderSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    products: [{
-        productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-        quantity: { type: Number, default: 1 }
-    }],
-    totalAmount: { type: Number, required: true },
-    status: { type: String, default: 'pending' },  // e.g., 'pending', 'processed', 'shipped', 'delivered'
-    orderDate: { type: Date, default: Date.now }
+// Define the order schema
+const OrderSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User', // This should match the name you use when you do mongoose.model('User', UserSchema);
+        required: true
+    },
+    products: [ProductSchema], // Array of product subdocuments
+    totalAmount: {
+        type: Number,
+        required: true
+    },
+    orderDate: {
+        type: Date,
+        default: Date.now
+    },
+    status: {
+        type: String,
+        enum: ['Pending', 'Shipped', 'Delivered', 'Canceled'],
+        default: 'Pending'
+    }
 });
 
-module.exports = mongoose.model('Order', orderSchema);
+// Create the model based on the schema
+const Order = mongoose.model('Order', OrderSchema);
 
+module.exports = Order;
--- models/productModel.js
+++ models/productModel.js
@@ -1,11 +1,52 @@
 const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    description: String,
-    image: String, // URL to the product image
-    stock: { type: Number, required: true, default: 0 }
+// Define the product schema
+const ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: [0, 'Price can not be less than 0.']
+    },
+    stockQuantity: {
+        type: Number,
+        required: true,
+        min: [0, 'Stock quantity can not be less than 0.']
+    },
+    imageUrl: {
+        type: String,
+        trim: true
+    },
+    category: {
+        type: String,
+        trim: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
-module.exports = mongoose.model('Product', productSchema);
+// Middleware to set updatedAt before product save
+ProductSchema.pre('save', function(next) {
+    this.updatedAt = Date.now();
+    next();
+});
+
+// Create the model based on the schema
+const Product = mongoose.model('Product', ProductSchema);
+
+module.exports = Product;
